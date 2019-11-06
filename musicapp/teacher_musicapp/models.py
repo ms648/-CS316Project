335f482c30894d2e891c77b351c01afd154de0c7
Shicheng Rao <sr307@duke.edu>
2019-11-06 12:39:05 -0500

    added sql files

--- musicapp/teacher_musicapp/models.py
+++ musicapp/teacher_musicapp/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 
 #this is where we write logic that corresponds with database -- business logic
+
+class Member(models.Model):
+	name =  models.CharField(max_length = 256)
+	email = models.CharField(max_length = 64)
+
+	def __str__(self):
+		return self.name
