713e64d68310aad8c6411dff21cb6c692f0d49a6
MacKenzi Simpson <ms648@duke.edu>
2019-11-06 11:05:44 -0500

    add path

--- musicapp/teacher_musicapp/views.py
+++ musicapp/teacher_musicapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 #THIS IS OUR CONTROLLER!!!!!!!!!!!
 #semi handles routing (with urls.py)
+
+#views are meant to take data from model (business logic) and return it to front end
+
+def index(request):
+    return HttpResponse("userIndex")
