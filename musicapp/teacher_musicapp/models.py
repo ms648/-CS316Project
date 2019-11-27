1e19176fadc033ab55382f34430acf5e4fbda435
Shicheng Rao <sr307@duke.edu>
2019-11-27 17:12:45 -0500

    test of html embedding

--- musicapp/teacher_musicapp/models.py
+++ musicapp/teacher_musicapp/models.py
@@ -43,7 +43,7 @@ class Trackable(models.Model):
 		db_table = "Trackables"
 
 	def __str__(self):
-		return self.name
+		return self.name + self.instrument
 
 class Recording(models.Model):
 	day = models.DateField()
